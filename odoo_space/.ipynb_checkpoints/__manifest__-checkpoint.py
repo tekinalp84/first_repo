b2030e6c367f63522f70137b0f0df026daebeb6a
tekinalp84 <onurtekinalp@gmail.com>
2021-03-25 16:52:01 +0000

    Space Mission

---
+++ odoo_space/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name' : 'Odoo Space',
+    
+    'summary': """Help Odoo get to the Moon""",
+    
+    'description': """
+    Odoo Inc is trying to visit the Moon
+    This app will help to organize the logistics
+    """,
+    
+    'author': 'Onur Tekinalp',
+    
+    'website': 'cube48.de',
+    
+    'category': 'Logistics',
+    
+    'version':'1.0',
+    
+    'depends': ['base'],
+    
+    'data':[
+        
+        
+    ],
+    
+    'demo':[
+        
+        
+    ],    
+    
+    
+    
+}
