f7c02dd8b3c3045067607fcc977681a48610d005
tekinalp84 <onurtekinalp@gmail.com>
2021-03-25 16:33:50 +0000

    Space mission

---
+++ odoo_spacemission/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name' : 'Odoo Space Mission',
+    
+    'summary': """Help Odoo get to the Moon""",
+    
+    'description': """
+    Odoo Inc is trying to visit the Moon
+    This app will help to organize the logistics
+    """,
+    
+    'author': 'Onur Tekinalp',
+    
+    'category': 'Logistics',
+    
+    'version':'1.0',
+    
+    'depends': ['base'],
+    
+    'data':[
+        
+        
+    ],
+    
+    'demo':[
+        
+        
+    ],    
+    
+    
+    
+}
---
+++ odoo_spacemission/models/spaceship.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Spaceship(models.Model):
+    _name ='spacemission.spaceship'
+    _definition = 'Spaceship'
