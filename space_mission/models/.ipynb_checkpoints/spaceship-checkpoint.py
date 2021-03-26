03a77b3851e408249a5b209f3357fac5c8d35f65
tekinalp84 <onurtekinalp@gmail.com>
2021-03-26 13:44:16 +0000

    Demo

--- space_mission/models/.ipynb_checkpoints/spaceship-checkpoint.py
+++ space_mission/models/.ipynb_checkpoints/spaceship-checkpoint.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Spaceship(models.Model):
     
@@ -13,11 +14,18 @@ class Spaceship(models.Model):
                                           ('exploration','Exploration'),
                                           ('cruise','Cruise')],
                                 copy=False)
-    dimensions = fields.Text(string='Dimensions')
+    length = fields.Float(string='Length', default=0.00)
+    width = fields.Float(string='Width', default=0.00)
     description = fields.Text(string='Description')
     fuel_type = fields.Text(string='Fuel Type')
     number_of_passengers = fields.Integer(string='Number of Passengers')
-    ship_image = fields.Binary(string="Ship Image")
+    ship_image = fields.Image(string="Ship Image")
     
     active = fields.Boolean(string='Active', default=True)
     
+    @api.constrains('length','width')
+    
+    def _check_ship_size(self):
+        for record in self:
+            if record.width > record.length:
+                raise UserError('Width can not be bigger than the lenght')
