8a830d0d57d7b7c0adee5b389dcbbd5a2fba8f84
tekinalp84 <onurtekinalp@gmail.com>
2021-03-26 15:23:17 +0000

    Space Mission

--- space_mission/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ space_mission/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -25,6 +25,7 @@
         'security/ir.model.access.csv',
         'views/space_mission_menuitems.xml',
         'views/spaceship_views.xml',
+        'views/mission_views.xml',
         
     ],
     
---
+++ space_mission/models/mission.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from datetime import timedelta
+
+class Mission(models.Model):
+    _name = 'space.mission'
+    _description = 'Space Missions'
+    
+    spaceship_id = fields.Many2one(comodel_name='space.spaceship',
+                                  string='Spaceship',
+                                  ondelete='cascade',
+                                  required=True)
+    
+    name = fields.Char(string='Mission', required=True)
+    
+    crew_members = fields.Many2many(comodel_name='res.partner',string='Crew Members')
+    
+    captain = fields.Many2one(comodel_name='res.partner', string='Captain')
+    
+    launch_date = fields.Date(string='Start Date',
+                            default=fields.Date.today)
+    
+    duration = fields.Integer(string='Duration',
+                             default=1)
+    
+    return_date = fields.Date(string='Return Date',
+                             compute='_compute_return_date',
+                             inverse='_inverse_end_date',
+                             store=True)
+    
+    @api.depends('launch_date','duration')
+    
+    def _compute_return_date(self):
+        for record in self:
+            if not (record.launch_date and record.duration):
+                record.return_date = record.launch_date
+            else:
+                duration_days = timedelta(days=record.duration)
+                record.return_date = record.launch_date + duration_days
+                
+    def _inverse_end_date(self):
+        for record in self:
+            if record.launch_date and record.return_date:
+                record.duration = (record.return_date - record.launch_date).days
+            else:
+                continue
